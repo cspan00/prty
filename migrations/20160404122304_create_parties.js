923625c0eac8cfa0b0748ec64f530edcaf7e49d6
Corey Yaspan <cspan00@gmail.com>
2016-04-04 12:30:45 -0600

    create parties and images tables

--- migrations/20160404122304_create_parties.js
+++ migrations/20160404122304_create_parties.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, promise) {
-  return knex.schema.createTable
+  return knex.schema.createTable('parties', function(t){
+    t.increments('id');
+    t.string('facebook_id');
+    t.bool('new');
+    t.timestamps();
+  })
 };
 
 exports.down = function(knex, Promise) {
-
+   return knex.schema.dropTableIfExists('parties')
 };
--- migrations/20160404122316_create_images.js
+++ migrations/20160404122316_create_images.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('images', function(t){
+    t.increments('id');
+    t.integer('party_id');
+    t.string('image_url');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('images')
 };
