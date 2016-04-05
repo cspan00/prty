941561d70aae125a108454f953a55b516f61aff3
Corey Yaspan <cspan00@gmail.com>
2016-04-05 08:41:00 -0600

    some styling

--- public/javascripts/controllers/urlController.js
+++ public/javascripts/controllers/urlController.js
@@ -7,7 +7,7 @@ app.controller('urlController', function($scope, $rootScope, $location, $http, u
       $http.get('new/'+facebook_id).then(function(response){
         var id = JSON.stringify(response['data'][0]['id']);
         $scope.path = "/#/current/" + id;
-        $scope.url ="go to prty.live/mobile/"+ id+ " on your phone to join!";
+        $scope.url ="go to prtylive.herokuapp/#/mobile/"+ id+ " on your phone to join!";
 
       })
 
