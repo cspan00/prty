471524be165c0214b3d87cb9bd847b0b2cfe6cce
Corey Yaspan <cspan00@gmail.com>
2016-04-07 11:08:55 -0600

    added some menu options

--- public/javascripts/controllers/partyController.js
+++ public/javascripts/controllers/partyController.js
@@ -1,21 +1,29 @@
-app.controller('partyController', function($scope, $rootScope, $location, $http, $interval, $routeParams, picService){
-var party_id = $routeParams.id
-$scope.stop = $interval( function() {$scope.getPics(); }, 2000);
-// code to stop $interval for the current party page when user continues through site.
-var dereg1 = $rootScope.$on('$locationChangeSuccess', function(){
-  $interval.cancel($scope.stop);
-  dereg1();
-});
-$scope.getPics = function(){
-  picService.getPics(party_id).then(function(results){
-    console.log(results);
-    $scope.pics = results;
-  })
-}
+app.controller('partyController', function($scope, $rootScope, $location, $http, $interval, $routeParams, picService, userService){
+    var party_id = $routeParams.id
+    $scope.stop = $interval( function() {$scope.getPics(); }, 2000);
+    // code to stop $interval for the current party page when user continues through site.
+    var dereg1 = $rootScope.$on('$locationChangeSuccess', function(){
+        $interval.cancel($scope.stop);
+        dereg1();
+      });
+    $scope.getPics = function(){
+        picService.getPics(party_id).then(function(results){
+          console.log(results);
+          $scope.pics = results;
+        })
+      }
 
+      $scope.logout = function(){
+        $auth.logout()
+        console.log("successfully logged out!");
+      }
 
+     $scope.user = function() {
+       userService.validateUser().then(function(response){
+         $scope.user = response
+       })
 
-}
+     }
 
 
 })
--- public/javascripts/jquery/showmenu.js
+++ public/javascripts/jquery/showmenu.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
-  alert("linked")
   $(document).keydown(function(e){
     if(e.keyCode == 13){
       $("#menu").toggle()
     }
-
   })
-
-
 })
