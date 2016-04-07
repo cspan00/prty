85e0dc2d7ed435f1cb7cd76af03276e688775ffa
Corey Yaspan <cspan00@gmail.com>
2016-04-06 18:04:12 -0600

    elements inline

--- public/javascripts/controllers/partyController.js
+++ public/javascripts/controllers/partyController.js
@@ -1,22 +1,17 @@
 app.controller('partyController', function($scope, $rootScope, $location, $http, $interval, $routeParams, picService){
 var party_id = $routeParams.id
-$scope.getPics = function(){
-  picService.getPics(party_id).then(function(results){
-    console.log(results);
-    $scope.pics = results;
-  })
-}
-
-
-
-$interval( function() {$scope.getPics(); }, 1000);
-
 $scope.stop = $interval( function() {$scope.getPics(); }, 2000);
 // code to stop $interval for the current party page when user continues through site.
 var dereg1 = $rootScope.$on('$locationChangeSuccess', function(){
   $interval.cancel($scope.stop);
   dereg1();
 });
+$scope.getPics = function(){
+  picService.getPics(party_id).then(function(results){
+    console.log(results);
+    $scope.pics = results;
+  })
+}
 
 
 
